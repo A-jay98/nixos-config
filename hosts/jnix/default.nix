375f586da6d40ca1694569ff41c6ce6f6ded5619
Ali Jamadi <jamadi1377@gmail.com>
2023-11-10 03:19:42 -0700

    added samba service

--- hosts/jnix/default.nix
+++ hosts/jnix/default.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./hardware-configuration.nix
+    ../../modules/services/samba.nix
   ];
 
   boot = {
--- modules/services/samba.nix
+++ modules/services/samba.nix
@@ -1,7 +1,41 @@
 { pkgs, vars, ... }:
 {
 
-services.
+  services.samba = {
+    enable = true;
+    # set up the user accounts to begin with:
+    # $ sudo smbpasswd -a yourusername
+    # This adds to the [global] section:
+    extraConfig = ''
+      browseable = no
+      smb encrypt = required
+      netbios name = morpheus
+      server string = morpheus
+      hosts allow = 10.0.
+      hosts deny = 0.0.0.0/0
+      guest account = nobody
+      map to guest = bad user
+    '';
+
+    shares = {
+      homes = {
+        browseable = "no"; # note: each home will be browseable; the "homes" share will not.
+        "read only" = "no";
+        "guest ok" = "no";
+      };
+      common-morpheus = {
+        path = "/mnt/Shares/Private";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "aj";
+        "force group" = "users";
+      };
+    };
+  };
+
 
 
 }
