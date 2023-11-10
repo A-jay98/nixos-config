fea28425bdf216e1c4e6d1cbc038458e72a31f6d
Ali Jamadi <jamadi1377@gmail.com>
2023-11-10 12:46:40 -0700

    added shiva's user

--- hosts/jnix/default.nix
+++ hosts/jnix/default.nix
@@ -11,7 +11,7 @@
     loader = {
       systemd-boot = {
         enable = true;
-        configurationLimit=5;
+        configurationLimit = 5;
       };
       timeout = 1;
       efi = {
@@ -24,7 +24,7 @@
   environment = {
     systemPackages = with pkgs; [
       # System Wide Packages
-
+      texlive.combined.scheme-full
     ];
   };
   networking.firewall.enable = false;
@@ -51,6 +51,12 @@
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZsVHhI2OjDFDl3t1ffvlOTZKZeoxoTH1NEcuQcV6uWVyBiWhsL5eJoGXC4VETMkxWwZtdMkDeFtFp/LVR20BoKAYgN9ge46DP+wStZbuoASzEu0cIupKnPxk2YLKr6VFJwE/jfeYY/MEm0C3givMfBxCZIGexWtvX9lm2+BO6QHuMO/bMKdKhIpGa4L6BYFj97EvIYzDSrk41rzq+p8mIOq6ESltmYDVlCjW6giJyX9bngSz+ZcKqEkU1nr30lDnyIXRSNFW2FJAkCm72dxWLfgx9hzyTsSiiuBz5b1ekVXFIYu7JiErOAlTsFA+P8CIx/nQ5Xnv5vNLpT5Z0W1rD Alis-MacBook-Pro"
   ];
 
-
+  # all other users settings
+  users.users."shiva" = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keyFiles = [
+      "/home/shiva/.ssh/authorizedKeys"
+    ];
+  };
 
 }
